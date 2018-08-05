421f285f20139387f275e080a42bc027fe7f58e5
Markus Sitzmann <markus.sitzmann@gmail.com>
2018-08-05 22:57:16 +0200

    updating project to newer chembience schema

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-groupadd -g $CHEMBIENCE_GID $CHEMBIENCE_USER && \
-    useradd --shell /bin/bash -u $CHEMBIENCE_UID -g $CHEMBIENCE_GID -o -c "" -M $CHEMBIENCE_USER
-
-export PYTHONPATH=/home/app:/home/share:$PYTHONPATH
+if ! id "app" >/dev/null 2>&1; then
+    groupadd -g $CHEMBIENCE_GID app && \
+    useradd --shell /bin/bash -u $CHEMBIENCE_UID -g $CHEMBIENCE_GID -o -c "" -M app
+fi
 
 exec "$@"
 
